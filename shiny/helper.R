dcd7f6397eb75b94e59080b7da750d324542ab52
bfrickert <bfrickert@yahoo.com>
2017-09-10 08:30:49 -0400

    Update helper.R

--- shiny/helper.R
+++ shiny/helper.R
@@ -1,36 +1,42 @@
-list.of.packages <- c("dplyr", "plotly")
+list.of.packages <- c("dplyr", "plotly", "RPostgreSQL")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
 
-library(dplyr)
 library(plotly)
 library(shiny)
+library(RPostgreSQL)
+library(dplyr)
 
-aggr <- read.csv('data/spark.aggregate.csv', sep=',', stringsAsFactors = F, header=T)
+
+dbname <-""
+user <- ""
+password <- scan(".pgpass", what="")
+host <- "127.0.0.1"
+con <- dbConnect(dbDriver("PostgreSQL"), user=user,
+                 password=password, dbname=dbname, host=host, port=5432)
+
+sql <- "select extract(month from logdate) mth, extract(year from logdate) yr, stream, sum(seconds) seconds, count(8) from (
+select logdate, 
+case when stream like ('do-or-diy%') then 'do-or-diy' when stream like 'freeform%' then 'freeform' when stream like 'drummer%' then 'drummer' when stream like 'ubu%' then 'ubu' when stream like 'ichiban%' then 'ichiban' end stream,
+seconds
+from wfmu.access_logs 
+where seconds > 0) access_logs
+where stream like ('do-or-diy%') or
+stream like ('drummer%') or
+stream like ('freeform%') or
+stream like ('ichiban%') or
+stream like ('ubu%')
+group by mth,yr,stream
+order by 2, 1, 3;"
+
+aggr <- dbGetQuery(con, sql)
+
 names(aggr) <- c('yr', 'mnth', 'channel', 'sum.ath', 'count.ath')
-aggr <- filter(aggr, !(is.na(yr)) & !grepl('.xsl', channel) & 
-                 !grepl('.aac', channel) & !grepl('admin/', channel) & 
-                 !grepl('.png', channel) & !grepl('.jpg', channel) &
-                 !grepl('.css', channel) & !grepl('.html', channel) &
-                 !grepl('.php', channel) & !grepl('.txt', channel) & 
-                 !grepl('.aspx', channel) & !grepl('.smi', channel) & 
-                 !grepl('.htm', channel) & !grepl('.ico', channel) & 
-                 !grepl('.xml', channel) & sum.ath > 100)
-aggr <- group_by(aggr, yr, mnth, channel) %>% 
-  summarise(sum.ath = sum(as.numeric(sum.ath)), count.ath = sum(as.numeric(count.ath))) %>% 
-  data.frame()
 aggr$sum.ath <- as.numeric(aggr$sum.ath)/60/60
 aggr$dt <- paste(aggr$yr, ' - ', aggr$mnth, sep='')
 
 choices <- unique(paste(aggr$yr, ' - ', aggr$mnth, sep=''))
 choices <- sort(choices, decreasing=T)
 
-counts.by.channel <- read.csv('data/counts.by.channel.aws.csv', stringsAsFactors = F, header=F)
-names(counts.by.channel) <- c('dt', 'hr', 'channel', 'cnt')
-counts.by.channel <- group_by(counts.by.channel, dt, hr, channel) %>% 
-  summarise(cnt = sum(cnt)) %>% 
-  data.frame()
-counts.by.channel$yr.mnth <- paste(substr(counts.by.channel$dt, 1,4), as.integer(substr(counts.by.channel$dt, 6,7)), sep=' - ')
-
-unique(aggr$channel)
+dbDisconnect(con)
